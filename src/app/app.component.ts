100e53eba5cf25164ebe85c89859ff2c6cdc7ab9
monigaur8 <mgaur1586@gmail.com>
2025-02-21 17:11:26 +0000

    update code

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { ContentComponent } from "./content/content.component";
+import { FooterComponent } from "./footer/footer.component";
+import { UserComponent } from "./user/user.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent, ContentComponent, FooterComponent, UserComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'taskManagementApp';
+}
